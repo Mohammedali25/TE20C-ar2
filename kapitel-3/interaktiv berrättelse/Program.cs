097e60ccc2062950d3d22bed4ea210778a444e64
Mohammedali25 <ali.mohammed@elev.ga.ntig.se>
2021-09-29 10:22:30 +0200

    tränat på datatyper och konventering

---
+++ kapitel-3/interaktiv berrättelse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace interaktiv_berrättelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
